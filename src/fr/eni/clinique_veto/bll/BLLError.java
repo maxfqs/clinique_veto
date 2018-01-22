b35efe67de07fb2465b7e6810db4fc4f55e045c5
maxfqs <maxfqs@gmail.com>
2018-01-22 16:13:44 +0100

    Fix formatting on BLLError

--- src/fr/eni/clinique_veto/bll/BLLError.java
+++ src/fr/eni/clinique_veto/bll/BLLError.java
@@ -9,20 +9,19 @@ public enum BLLError {
 	INVALID_PERSONNEL_ROLE(100, "Le rôle est incorrect"),
 	FAILED_PERSONNEL_ADD(101, "Erreur lors de l'ajout de ce personnel"),
 	FAILED_PERSONNEL_UPDATE(102, "Erreur lors de la sauvegarde du personnel"),
-		
+	
+	// 200 - Client
+	FAILED_CLIENT_ADD(201, "Erreur lors de l'ajout de ce client"),
+	FAILED_CLIENT_UPDATE(202, "Erreur lors de la sauvegarde du client"),
+	FAILED_CLIENT_GET(203, "Erreur lors de la recherche du client"),
+	
 	// 300 - Animal
 	INVALID_ANIMAL_ESPECES(300, "L'espèce est incorrect"),
 	INVALID_ANIMAL_RACES(301, "La race est incorrect"),
 	INVALID_ANIMAL_SEXES(302, "Le sexe est incorrect"),
 	INVALID_ANIMAL_CLIENT_ID(303, "Le numéro de client est incorrect"),
 	FAILED_ANIMAL_ADD(304, "Erreur lors de l'ajout de cet animal"),
-	FAILED_ANIMAL_UPDATE(305, "Erreur lors de la sauvegarde de cet animal"),
-	
-	// 200 - Personnel
-	FAILED_CLIENT_ADD(201, "Erreur lors de l'ajout de ce client"),
-	FAILED_CLIENT_UPDATE(202, "Erreur lors de la sauvegarde du clientl"),
-	FAILED_CLIENT_GET(203, "Erreur lors de la recherche du client");
-	
+	FAILED_ANIMAL_UPDATE(305, "Erreur lors de la sauvegarde de cet animal"),	
 	
 	;
 	
